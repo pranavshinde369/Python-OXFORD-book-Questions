ca6fe43519f16f5fcc4f188a6e0eb6b74a84cae7
Pranav Shinde <shindepranav369@gmail.com>
2026-04-05 10:42:27 +0530

    Completed Chapter 4

--- 01_main.py
+++ 01_main.py
@@ -158,9 +158,105 @@ for i in range(1, n+1):
     total += (i**2) / i
 print(f"Sum of series is: {total}")
 
-#Q14 Write a program that prints a number, its square, and cube repeatedly in the range (1,n).
+# Q14 Write a program that prints a number, its square, and cube repeatedly in the range (1,n).
 
 n = int(input("Enter a number: "))
 for i in range (1, n+1):
     print(i,i**2,i**3)
-    
+
+# Q15 Write a program that prompts the user to enter a string. The program calculates and  
+# display the length of string until the user enters QUIT (Use While Loop)
+
+while True:
+    text = input("Enter a string (type QUIT to exit): ") 
+    if text.upper() == "QUIT":
+        break
+    print(f"length of string:", len(text))
+
+# Q16 Write a program that prompts user to enter numbers. Once the user enters -1, 
+# it displays the count, sum, and average of even numbers and that of odd numbers.
+
+count_even = 0
+count_odd = 0
+sum_even = 0
+sum_odd = 0
+
+while True:
+    num = int(input("Enter a number (-1 to stop): "))
+
+    if num == -1:
+        break
+
+    if num % 2 == 0:
+        count_even += 1
+        sum_even += num
+    else:
+        count_odd += 1
+        sum_odd += num
+
+avg_even = sum_even / count_even if count_even != 0 else 0
+avg_odd = sum_odd / count_odd if count_odd != 0 else 0
+
+print("Even Numbers -> Count:", count_even, "Sum:", sum_even, "Average:", avg_even)
+print("Odd Numbers  -> Count:", count_odd, "Sum:", sum_odd, "Average:", avg_odd)\
+
+# Q17 Write a program to calculate electricity bill.
+
+bill = int(input("Enter Consumed unit: "))
+
+if bill <= 150:
+    amount = bill * 3
+elif bill <= 300:
+    amount = (150 * 3) + (bill - 150) * 3.75
+elif bill <= 450:
+    amount = (150 * 3) + (150 * 3.75) + (bill - 300) * 4
+elif bill <= 600:
+    amount = (150 * 3) + (150 * 3.75) + (150 * 4) + (bill - 450) * 4.25
+else:
+    amount = (150 * 3) + (150 * 3.75) + (150 * 4) + (150 * 4.25) + (bill - 600) * 5
+print("Your Electricity bill is:", amount)
+
+# Q18 Generate patterns      
+# 🔍 Logic: It’s a 5×5 grid,   We print * only on the border,   Inside is empty (spaces)
+# 📌 Conditions:   First row → i == 0   Last row → i == 4   First column → j == 0   Last column j == 4
+#   *****
+#   *   *
+#   *   *
+#   *   *
+#   *****
+
+for i in range(5):
+    for j in range(5):
+        if i == 0 or i == 4 or j == 0 or j == 4:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+# Q19 Generate pattern 
+#   $ * * * *
+#   * $ * * *
+#   * * $ * *
+#   * * * $ *
+#   * * * * $
+
+n = 5
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            print("$", end=" ")
+        else:
+            print("*", end=" ")
+    print()
+
+
+# Q20 A video library rents new video for $75 a day, and old movies for $50 a day. Write a program to
+# calculate the total charge for a customer's video rentals. The Program should prompt the user for no of 
+# each type of video and outputr the totgal cost
+
+new_videos = int(input("Enter number of new videos: "))
+old_movies = int(input("Enter number of old movies: "))
+
+total_cost = (new_videos * 75) + (old_movies * 50)
+
+print("Total cost is ₹", total_cost)
