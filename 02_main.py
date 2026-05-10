0245cb1970b943439b86e03264ddfee57084c820
Pranav Shinde <shindepranav369@gmail.com>
2026-05-10 12:54:21 +0530

    5 qns done for today

--- 02_main.py
+++ 02_main.py
@@ -66,6 +66,61 @@ def month_name(month):
     else:
         print("Invalid month number")
 
+monthno = int(input("Enter month number"))
+month_name(monthno)
 
-# Q26 Write a program to concatenate two strings using recursion.
+# Q26 Write a function that accepts a number n as input and returns 
+#     the average of numbers from 1 to n 
 
+def num_avg(n):
+    num=0
+    for i in range(0,n+1):
+        num += i
+    return num/n
+
+numm = int(input("enter a number"))
+print(num_avg(numm))
+
+# Q27 Write a program to calculate te area of triangle using a function
+
+def traiangle_area(base,height):
+    return 1/2 * base * height
+
+print(traiangle_area(3,4))
+
+# Q28 Write a program using function that returns the surface area and volume of a sphere.
+
+def sphere(radius):
+    surface_area = 4 * 3.141 * radius**2
+    volume_sphere = 4/3 * 3.141 * radius**3
+    print("Surface area of sphere =",surface_area)
+    print("Volume of sphere =",volume_sphere)
+
+sphere(5)
+
+
+# Q29 Write a Program to reverse a string without recursion
+
+string = input("enter a string:")
+
+reverse_string = string[::-1]
+print("reversed string:", reverse_string)
+
+# Q30 Write a function to print a table of binomial coefficients B(m,x) = m! / (x!(m-x)!) where m>x
+
+def factorial(n):  #function to calculate factorial
+    fact = 1
+    for i in range(1,n+1):
+        fact = fact * i
+    return fact
+
+def binomial(m, x):
+    
+    return factorial(m) // (factorial(x) * factorial(m - x))
+
+n = int(input("Enter number of rows: "))  # Printing table of binomial coefficients
+
+for m in range(n + 1):
+    for x in range(m + 1):
+        print(binomial(m, x), end=" ")
+    print()
